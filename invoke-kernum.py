77bf5d0afacc694677ec0329aa54d3c8bf8cfdc2
lyethar <107083757+lyethar@users.noreply.github.com>
2023-03-06 11:44:27 -0500

    Update invoke-kernum.py

--- invoke-kernum.py
+++ invoke-kernum.py
@@ -43,11 +43,10 @@ def invokeKerbrute():
 		os.system('./kerbrute_linux_amd64 userenum -d ' + domain + ' ' + l + '>> valid_users.txt' )
 
 def invokeFormat():
-	os.system("""cat valid_users.txt | cut -d "      " -f2 > valid_users2.txt""")
-	os.system("""cat valid_users2.txt | tr -d "[:blank:]" > valid_users3.txt""")
+	os.system("""cat valid_users.txt | cut -d " " -f14 > valid_users2.txt""")
 	print(Fore.RED + "\nCheck userlist and remove faulty users!")
 	print(Style.RESET_ALL)
-	os.system("cat valid_users3.txt" +  " |  cut -f1 -d" + "@" + " | tee userlist")
+	os.system("cat valid_users2.txt" +  " |  cut -f1 -d" + "@" + " | tee userlist")
 printBanner() 
 print(Style.RESET_ALL)
 
